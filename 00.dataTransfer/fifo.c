2126cd726341459663bfa43cd1682df72c21d75a
Seyoung Cheon <cheon798@gmail.com>
2019-11-29 19:58:07 +0900

    add fifo

--- 00.dataTransfer/fifo.c
+++ 00.dataTransfer/fifo.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define FIFO_FILENAME   "./testfifo"
 
@@ -19,25 +20,38 @@ doWrite(const char *message)
 {
     int fd, rtn;
 
-    fd = open(FIFO_FILENAME, O_WRONLY);
-    if (fd == -1) {
-        perror("open() \n");
+    rtn = unlink(FIFO_FILENAME);
+    // if (rtn == -1) {
+    //     perror("unlink() \n");
+    //     return -1;
+    // }
+
+    rtn = mkfifo(FIFO_FILENAME, 0644);
+    if (rtn == -1) {
+        perror("mkfifo() fail!\n");
         return -1;
     }
 
+    fd = open(FIFO_FILENAME, O_WRONLY);
+    if (fd == -1) {
+        perror("open() fail!\n");
+        return -1;
+    }
+
+    printf("size of message is %ld \n", sizeof(message));
     rtn = write(fd, message, sizeof(message));
     if (rtn = -1) {
-        perror("write() \n");
+        perror("write() fail!\n");
         rtn = close(fd);
         if (rtn == -1) {
-            perror("close() \n");
+            perror("close() fail!\n");
             return -1;
         }
     }
 
     rtn = close(fd);
     if (rtn == -1) {
-        perror("close() \n");
+        perror("close() fail!\n");
         return -1;
     }
 
@@ -54,25 +68,26 @@ doRead(void)
 
     fd = open(FIFO_FILENAME, O_RDONLY);
     if (fd == -1) {
-        perror("open() \n");
+        perror("open() fail!\n");
         return -1;
     }
 
     rtn = read(fd, rbuf, sizeof(rbuf));
     if (rtn = -1) {
-        perror("read() \n");
+        perror("read() fail!\n");
         rtn = close(fd);
         if (rtn == -1) {
-            perror("close() \n");
+            perror("close() fail!\n");
             return -1;
         }
+        return -1;
     }
 
     printf("Writer said... %s \n", rbuf);
 
     rtn = close(fd);
     if (rtn == -1) {
-        perror("close() \n");
+        perror("close() fail!\n");
         return -1;
     }
 
@@ -84,29 +99,46 @@ doRead(void)
 int 
 main(int argc, char const *argv[])
 {
-    int rtn;
+    int fd, rtn;
+    char wbuf[2048];
 
-    if (argc < 3) {
+    if (argc < 2) {
         print_usage(argv[0]);
         return -1;
     }
 
-    rtn = unlink(FIFO_FILENAME);
-    // if (rtn == -1) {
-    //     perror("unlink() \n");
-    //     return -1;
-    // }
-
-    rtn = mkfifo(FIFO_FILENAME, 0644);
-    if (rtn == -1) {
-        perror("mkfifo() \n");
-        return -1;
-    }
-
-    if (argv[1] == "w") {
+    if (!strcmp(argv[1], "w")) {
         /* writer */
-        doWrite(argv[2]);
-    } else if (argv[1] == "s") {
+        rtn = unlink(FIFO_FILENAME);
+
+        rtn = mkfifo(FIFO_FILENAME, 0644);
+        if (rtn == -1) {
+            perror("mkfifo() fail!\n");
+            return -1;
+        }
+
+        fd = open(FIFO_FILENAME, O_WRONLY);
+        if (fd == -1) {
+            perror("open() fail!\n");
+            return -1;
+        }
+
+        strcpy(wbuf, argv[2]);
+        printf("size of wbuf is %ld \n", sizeof(wbuf));
+        printf("size of argv[2] is %ld \n", sizeof(argv[2]));
+
+        rtn = write(fd, wbuf, sizeof(wbuf));
+        if (rtn = -1) {
+            perror("write() fail!\n");
+            printf("errno is %s \n", strerror(errno));
+            rtn = close(fd);
+            if (rtn == -1) {
+                perror("close() fail!\n");
+                return -1;
+            }
+            return -1;
+        }       
+    } else if (!strcmp(argv[1], "s")) {
         /* reader */
         doRead();
     } else {
